7768ca863f79142fee8a75608003ac853afd13e9
cyantis <cyantis@gmail.com>
2019-08-26 09:44:09 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(arr, str){
+  return arr.filter(function (i) { return i.toLowerCase() === str.toLowerCase(); } );
+}
+
+function fuzzyMatch(arr, str){
+  return arr.filter(function (i) { return i.startsWith(str); } );
+}
+
+function matchName(arr, str){
+  return arr.filter(function (i) { return i.name === str; } );
+}
