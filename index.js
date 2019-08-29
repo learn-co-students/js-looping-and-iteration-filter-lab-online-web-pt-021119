657280541797d505274b06eb71353378752bdb97
Eyal Toledano <eutait@gmail.com>
2019-08-28 22:19:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+
+function findMatching(objects, name) {
+  const result = objects.filter(function (n) {
+    return n.toLowerCase() == name.toLowerCase();
+  });
+  return result;
+};
+
+function fuzzyMatch(objects, query) {
+  const result = objects.filter(function (n) {
+    return n.slice(0,1).includes(query.slice(0,1));
+  });
+  return result;
+};
+
+function matchName(objects, name) {
+  const result = objects.filter(function (n) {
+    return n.name == name;
+  });
+  return result;
+};
